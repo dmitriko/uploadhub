ac1569224ccddd10693a66f2b97fb1a74faf9198
DmitriKo <dmitrikozhevin@gmail.com>
2023-11-11 18:10:32 +0200

    uploaded file model

--- fileportal/uploadhub/models.py
+++ fileportal/uploadhub/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class UploadedFile(models.Model):
+    original_name = models.CharField(max_length=255)
+    content_type = models.CharField(max_length=50)
+    unique_id = models.UUIDField(default=uuid.uuid4, 
+                                 editable=False, 
+                                 unique=True)
