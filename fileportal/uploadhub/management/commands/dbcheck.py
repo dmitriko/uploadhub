e7f4adc0d2071910ad00f794baf564976f94c59b
DmitriKo <dmitrikozhevin@gmail.com>
2023-11-11 22:07:57 +0200

    better dbcheck

--- fileportal/uploadhub/management/commands/dbcheck.py
+++ fileportal/uploadhub/management/commands/dbcheck.py
@@ -1,3 +1,4 @@
+# dbcheck.py
 from django.core.management.base import BaseCommand
 from django.db import connections
 from django.db.utils import OperationalError
@@ -6,13 +7,11 @@ class Command(BaseCommand):
     help = 'Checks database availability'
 
     def handle(self, *args, **options):
-        db_conn = None
         try:
             db_conn = connections['default']
             db_conn.cursor()
         except OperationalError:
             self.stdout.write('Database unavailable, waiting 1 second...')
-            return 1  # Return a non-zero exit code to indicate failure
+            return
         else:
             self.stdout.write(self.style.SUCCESS('Database is available'))
-            return 0  # Return zero to indicate success
